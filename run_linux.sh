f686eb475bc3f2125c33071e3d039af0def40272
quuuut <>
2025-10-10 16:34:21 +1030

    increase delay in linux tester and do debugging in macro

--- run_linux.sh
+++ run_linux.sh
@@ -1,7 +1,7 @@
 sudo su -c "" # Get root permissions for kernel to cache
 
 wine ./visual\ studio/x64/Release/suspend.exe &
-sleep 0.5
+sleep 0.6
 sudo ./visual\ studio/Resource\ Files/Suspend_Input_Helper_Linux_Binary suspend.exe & 
 
 read blank
